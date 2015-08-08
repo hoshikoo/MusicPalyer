d55c2062c032289d042cfe686e93eaf7bec97412
hoshikoo <hoshikooki@gmail.com>
2015-08-08 14:58:58 -0400

    initial commit

---
+++ app/src/main/java/hoshikoo/c4q/nyc/musicplayer/MusicController.java
@@ -0,0 +1,17 @@
+package hoshikoo.c4q.nyc.musicplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+/**
+ * Created by Hoshiko on 8/8/15.
+ */
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
---
+++ app/src/main/java/hoshikoo/c4q/nyc/musicplayer/Song.java
@@ -0,0 +1,21 @@
+package hoshikoo.c4q.nyc.musicplayer;
+
+/**
+ * Created by Hoshiko on 8/8/15.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist){
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
